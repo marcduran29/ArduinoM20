c465853d23489a89dd547d4bfdf4fa2d00f6212c
Bryansalesians <71720279+Bryansalesians@users.noreply.github.com>
2021-03-22 20:20:44 +0100

    prueba

--- ArduinoBalisesActives/ArduinoBalisesActives/Form1.Designer.cs
+++ ArduinoBalisesActives/ArduinoBalisesActives/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace ArduinoBalisesActives
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(415, 136);
+            this.button1.Location = new System.Drawing.Point(217, 69);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
